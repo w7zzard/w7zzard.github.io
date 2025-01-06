62da60a89613e4dd70c390001e4bb24557673a5d
W7zzard <1hoodgamer@gmail.com>
2025-01-06 18:47:11 -0500

    Updates

---
+++ precache-manifest.36e801c7c9687c5a31ed0044d80bdb30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d809e1fae036d6676796d885e2978d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ee58526833f5082ee7f",
+    "url": "/static/css/2.58736d6f.chunk.css"
+  },
+  {
+    "revision": "21d53dd83de0dd94f19a",
+    "url": "/static/css/main.df700d1f.chunk.css"
+  },
+  {
+    "revision": "2ee58526833f5082ee7f",
+    "url": "/static/js/2.f2ba9180.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.f2ba9180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d53dd83de0dd94f19a",
+    "url": "/static/js/main.497bf798.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
